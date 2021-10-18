7f4cc425af10c1e17c6378c04f6ae991050edff2
Adrien Bethuel <adrien.bethuel@eleve.isep.fr>
2021-10-18 23:48:58 +0200

    started connexion page

---
+++ connexion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style_connexion_inscription.css">
+        <title>Connexion</title>
+    </head>
+
+    <body>
+        <main>  <!-- On place le contenu de la page connexion/inscription dans le header-->
+            <div class="wrapper">
+                <h1 class="aircare">AirCare</h1>
+                <div class="content"> <!-- Contenu du bloc de connexion/inscription -->
+                    <h1 class="connexion">Connexion</h1>
+                    <p class="subtitle">Ceci est du texte</p>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
