aac07da876011cea3e15320c73de879cdd237423
Adrien Bethuel <adrien.bethuel@eleve.isep.fr>
2021-10-19 00:02:01 +0200

    favicon

--- connexion.php
+++ connexion.php
@@ -4,13 +4,15 @@
     <head>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="style_connexion_inscription.css">
+        <link rel="icon" href="./Assets/images/logo.ico"/> <!-- icone du site onglet du navigateur -->
         <title>Connexion</title>
     </head>
 
     <body>
+
         <main>  <!-- On place le contenu de la page connexion/inscription dans le header-->
             <div class="wrapper">
-                <h1 class="aircare">AirCare</h1>
+                <h1 class="aircare">AirCare</h1> <!-- Titre de page -->
                 <div class="content"> <!-- Contenu du bloc de connexion/inscription -->
                     <h1 class="connexion">Connexion</h1>
                     <p class="subtitle">Ceci est du texte</p>
@@ -18,4 +20,5 @@
             </div>
         </main>
     </body>
+
 </html>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="style_index.css">
+        <link rel="icon" href="./Assets/images/logo.ico"/> <!-- icone du site onglet du navigateur -->
         <title>Titre test</title>
     </head>
 
