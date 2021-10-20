a67eaa32b032ce1bec55cf19ce0f8d7fe78513ec
Adrien Bethuel <adrien.bethuel@eleve.isep.fr>
2021-10-21 00:23:15 +0200

    started header

---
+++ Components/header.php
@@ -0,0 +1,22 @@
+<!-- header.php 
+
+Header des différentes pages à ajouter à chaque page le contenent -->
+
+<nav>
+    <div class="left">
+        <div>
+            <a><img class="logo" src="../Assets/images/logo.png"></a>
+        </div>
+        <div class="menu">
+            <a href="../index.php">ACCEUIL</a>
+            <a href="../information.php">INFORMATIONS</a>
+            <a href="../liste_capteurs.php">CAPTEURS</a>
+            <a href="../forum_sujet.php">FORUM</a>
+            <a href="../quiz.php">QUIZ</a>
+        </div>
+    </div>
+</nav>
+
+
+
+
--- index.php
+++ index.php
@@ -3,10 +3,13 @@
     <head>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="style_index.css">
+        <link rel="icon" href="./Assets/images/logo.ico"/> <!-- icone du site onglet du navigateur -->
         <title>Titre test</title>
     </head>
 
     <body>
+
+        <?php include_once('./Components/header.php'); ?>
         <div class="section1">
             <h1>Test Site test</h1>
         </div>
