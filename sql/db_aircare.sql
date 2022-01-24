dea252020aaaac216787d23538f9b67e529ffa13
Adrien Bethuel <adrien.bethuel@eleve.isep.fr>
2022-01-25 00:02:18 +0100

    test db

--- sql/db_aircare.sql
+++ sql/db_aircare.sql
@@ -22,14 +22,14 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
-CREATE DATABASE `db_aircare` ;
+CREATE DATABASE `gywulo_aircare` ;
 
 --
 -- Structure de la table `air_quality`
 --
 
-DROP TABLE IF EXISTS `db_aircare`.`air_quality`;
-CREATE TABLE IF NOT EXISTS `db_aircare`.`air_quality` (
+DROP TABLE IF EXISTS `gywulo_aircare`.`air_quality`;
+CREATE TABLE IF NOT EXISTS `gywulo_aircare`.`air_quality` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_captor` int NOT NULL,
   `id_particles` int NOT NULL,
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS `db_aircare`.`air_quality` (
 -- Déchargement des données de la table `air_quality`
 --
 
-INSERT INTO `db_aircare`.`air_quality` (`id`, `id_captor`, `id_particles`, `id_monoxide`, `id_nitrogen`, `place`, `result`, `quality`, `date`) VALUES
+INSERT INTO `gywulo_aircare`.`air_quality` (`id`, `id_captor`, `id_particles`, `id_monoxide`, `id_nitrogen`, `place`, `result`, `quality`, `date`) VALUES
 (1, 2, 1, 1, 1, 'Salle d\'hospitalisation', 100, 'Mauvaise', '2010'),
 (2, 2, 1, 1, 1, 'Salle d\'hospitalisation', 95, 'Mauvaise', '2011'),
 (3, 2, 1, 1, 1, 'Salle d\'hospitalisation', 91, 'Mauvaise', '2012'),
@@ -61,8 +61,8 @@ INSERT INTO `db_aircare`.`air_quality` (`id`, `id_captor`, `id_particles`, `id_m
 -- Structure de la table `bpm`
 --
 
-DROP TABLE IF EXISTS `db_aircare`.`bpm`;
-CREATE TABLE IF NOT EXISTS `db_aircare`.`bpm` (
+DROP TABLE IF EXISTS `gywulo_aircare`.`bpm`;
+CREATE TABLE IF NOT EXISTS `gywulo_aircare`.`bpm` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_user` int NOT NULL,
   `value` int NOT NULL,
@@ -74,7 +74,7 @@ CREATE TABLE IF NOT EXISTS `db_aircare`.`bpm` (
 -- Déchargement des données de la table `bpm`
 --
 
-INSERT INTO `db_aircare`.`bpm` (`id`, `id_user`, `value`, `quality`) VALUES
+INSERT INTO `gywulo_aircare`.`bpm` (`id`, `id_user`, `value`, `quality`) VALUES
 (1, 16, 85, 'normale'),
 (2, 16, 108, 'élevé');
 
@@ -84,8 +84,8 @@ INSERT INTO `db_aircare`.`bpm` (`id`, `id_user`, `value`, `quality`) VALUES
 -- Structure de la table `captors`
 --
 
-DROP TABLE IF EXISTS `db_aircare`.`captors`;
-CREATE TABLE IF NOT EXISTS `db_aircare`.`captors` (
+DROP TABLE IF EXISTS `gywulo_aircare`.`captors`;
+CREATE TABLE IF NOT EXISTS `gywulo_aircare`.`captors` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
@@ -101,7 +101,7 @@ CREATE TABLE IF NOT EXISTS `db_aircare`.`captors` (
 -- Déchargement des données de la table `captors`
 --
 
-INSERT INTO `db_aircare`.`captors` (`id`, `title`, `description`, `place`, `date`, `image`, `user_id`, `user_name`) VALUES
+INSERT INTO `gywulo_aircare`.`captors` (`id`, `title`, `description`, `place`, `date`, `image`, `user_id`, `user_name`) VALUES
 (1, 'Air champ de vigne', 'Nous cherchons à déterminer la qualité de l\'air dans l\'environnement des champs de vignes.', 'Bordeaux, France', '12/12/2012', 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg', 13, 'Jean Dupont'),
 (2, 'Salle d\'hospitalisation', 'Nous cherchons à déterminer si la salle d\'hospitalisation bénéficie d\'une qualité de l\'air suffisante afin d\'accueillir les patients.', 'Hôpital Cochin', '20/09/1312', 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg', 15, 'Adrien Bethuel'),
 (3, 'Capteur salle de classe', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vulputate pretium egestas. Nulla facilisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam luctus fringilla est elementum pretium. In eleifend tortor ac rhoncus rutrum. Aliquam nec nibh pharetra, semper eros vitae, tempus elit. Aliquam porta tincidunt justo, vel aliquam ante hendrerit hendrerit. Nunc vestibulum justo risus, in eleifend est luctus et.\r\n\r\nCras iaculis, mi id ultrices volutpat, sem nibh tincidunt nisi, eu tempor turpis ipsum ac erat. In ut justo sed nisi lacinia interdum in quis quam. Maecenas luctus odio et erat porta facilisis. Proin ut massa mollis turpis tempor vulputate eget dignissim dui. Vivamus pellentesque diam tristique erat sollicitudin dictum. Nullam semper felis mi, ac vestibulum lectus dignissim eget. Integer a imperdiet tortor, ac ultrices nisl. Suspendisse ac enim tortor. In non odio quis augue blandit tempus ac at libero. Nunc massa mi, aliquam vitae egestas et, mollis in orci. Nam facilisis turpis sed erat volutpat, in pellentesque ipsum sagittis. Quisque a nulla a lectus semper dictum. Etiam vel elementum mi, et porttitor erat. Curabitur in magna vitae elit fringilla porta. Praesent in convallis ex, sit amet blandit elit. Vestibulum ut volutpat tellus.\r\n\r\nQuisque sagittis turpis in posuere pretium. Quisque molestie aliquam tellus quis rhoncus. In non imperdiet massa, eu tristique nisi. Praesent at elementum massa. Proin cursus, sapien sed faucibus varius, velit eros pulvinar mi, vitae scelerisque elit orci nec elit. Maecenas ultrices convallis laoreet. Fusce lacinia sollicitudin ligula, ut consequat tur', 'Ecole Victor Hugo', '01/01/2001', 'https://static.lpnt.fr/images/2021/10/14/22319050lpw-22319058-article-jpg_8301796_660x287.jpg', 6, 'Prénom Nom');
@@ -112,8 +112,8 @@ INSERT INTO `db_aircare`.`captors` (`id`, `title`, `description`, `place`, `date
 -- Structure de la table `carbon_monoxide`
 --
 
-DROP TABLE IF EXISTS `db_aircare`.`carbon_monoxide`;
-CREATE TABLE IF NOT EXISTS `db_aircare`.`carbon_monoxide` (
+DROP TABLE IF EXISTS `gywulo_aircare`.`carbon_monoxide`;
+CREATE TABLE IF NOT EXISTS `gywulo_aircare`.`carbon_monoxide` (
   `id` int NOT NULL AUTO_INCREMENT,
   `captor_id` int NOT NULL,
   `value` float NOT NULL,
@@ -125,7 +125,7 @@ CREATE TABLE IF NOT EXISTS `db_aircare`.`carbon_monoxide` (
 -- Déchargement des données de la table `carbon_monoxide`
 --
 
-INSERT INTO `db_aircare`.`carbon_monoxide` (`id`, `captor_id`, `value`, `percentage`) VALUES
+INSERT INTO `gywulo_aircare`.`carbon_monoxide` (`id`, `captor_id`, `value`, `percentage`) VALUES
 (1, 2, 30, 100);
 
 -- --------------------------------------------------------
@@ -134,8 +134,8 @@ INSERT INTO `db_aircare`.`carbon_monoxide` (`id`, `captor_id`, `value`, `percent
 -- Structure de la table `email`
 --
 
-DROP TABLE IF EXISTS `db_aircare`.`email`;
-CREATE TABLE IF NOT EXISTS `db_aircare`.`email` (
+DROP TABLE IF EXISTS `gywulo_aircare`.`email`;
+CREATE TABLE IF NOT EXISTS `gywulo_aircare`.`email` (
   `id` int NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `sujet` varchar(255) NOT NULL,
@@ -148,7 +148,7 @@ CREATE TABLE IF NOT EXISTS `db_aircare`.`email` (
 -- Déchargement des données de la table `email`
 --
 
-INSERT INTO `db_aircare`.`email` (`id`, `email`, `sujet`, `message`, `status`) VALUES
+INSERT INTO `gywulo_aircare`.`email` (`id`, `email`, `sujet`, `message`, `status`) VALUES
 (3, 'max.verstappen@gmail.com', 'Sujet', 'Message', 'seen');
 
 -- --------------------------------------------------------
@@ -157,8 +157,8 @@ INSERT INTO `db_aircare`.`email` (`id`, `email`, `sujet`, `message`, `status`) V
 -- Structure de la table `faq`
 --
 
-DROP TABLE IF EXISTS `db_aircare`.`faq`;
-CREATE TABLE IF NOT EXISTS `db_aircare`.`faq` (
+DROP TABLE IF EXISTS `gywulo_aircare`.`faq`;
+CREATE TABLE IF NOT EXISTS `gywulo_aircare`.`faq` (
   `id` int NOT NULL AUTO_INCREMENT,
   `question` varchar(255) NOT NULL,
   `answer` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
@@ -169,7 +169,7 @@ CREATE TABLE IF NOT EXISTS `db_aircare`.`faq` (
 -- Déchargement des données de la table `faq`
 --
 
-INSERT INTO `db_aircare`.`faq` (`id`, `question`, `answer`) VALUES
+INSERT INTO `gywulo_aircare`.`faq` (`id`, `question`, `answer`) VALUES
 (1, 'Lorem Ipsim', 'Lorem Ipsum reponse'),
 (2, 'Nouvelle question ', 'Réponse à la question');
 
@@ -179,8 +179,8 @@ INSERT INTO `db_aircare`.`faq` (`id`, `question`, `answer`) VALUES
 -- Structure de la table `fine_particles`
 --
 
-DROP TABLE IF EXISTS `db_aircare`.`fine_particles`;
-CREATE TABLE IF NOT EXISTS `db_aircare`.`fine_particles` (
+DROP TABLE IF EXISTS `gywulo_aircare`.`fine_particles`;
+CREATE TABLE IF NOT EXISTS `gywulo_aircare`.`fine_particles` (
   `id` int NOT NULL AUTO_INCREMENT,
   `captor_id` int NOT NULL,
   `value` float NOT NULL,
@@ -192,7 +192,7 @@ CREATE TABLE IF NOT EXISTS `db_aircare`.`fine_particles` (
 -- Déchargement des données de la table `fine_particles`
 --
 
-INSERT INTO `db_aircare`.`fine_particles` (`id`, `captor_id`, `value`, `percentage`) VALUES
+INSERT INTO `gywulo_aircare`.`fine_particles` (`id`, `captor_id`, `value`, `percentage`) VALUES
 (1, 2, 35, 100);
 
 -- --------------------------------------------------------
@@ -201,8 +201,8 @@ INSERT INTO `db_aircare`.`fine_particles` (`id`, `captor_id`, `value`, `percenta
 -- Structure de la table `messages`
 --
 
-DROP TABLE IF EXISTS `db_aircare`.`messages`;
-CREATE TABLE IF NOT EXISTS `db_aircare`.`messages` (
+DROP TABLE IF EXISTS `gywulo_aircare`.`messages`;
+CREATE TABLE IF NOT EXISTS `gywulo_aircare`.`messages` (
   `id` int NOT NULL AUTO_INCREMENT,
   `content` text NOT NULL,
   `date` text NOT NULL,
@@ -216,7 +216,7 @@ CREATE TABLE IF NOT EXISTS `db_aircare`.`messages` (
 -- Déchargement des données de la table `messages`
 --
 
-INSERT INTO `db_aircare`.`messages` (`id`, `content`, `date`, `user_id`, `user_name`, `topic_id`) VALUES
+INSERT INTO `gywulo_aircare`.`messages` (`id`, `content`, `date`, `user_id`, `user_name`, `topic_id`) VALUES
 (5, 'Refaisons un test de réponse<br />\r\nCeci est une réponse<br />\r\n', '17/12/2021 à 10:02', 15, 'Adrien Bethuel', 13),
 (2, 'Test de réponse', '17/12/2021 à 09:33', 15, 'Adrien Bethuel', 13),
 (6, 'Réponse à ce topic', '17/12/2021 à 10:04', 15, 'Adrien Bethuel', 5),
@@ -231,8 +231,8 @@ INSERT INTO `db_aircare`.`messages` (`id`, `content`, `date`, `user_id`, `user_n
 -- Structure de la table `news`
 --
 
-DROP TABLE IF EXISTS `db_aircare`.`news`;
-CREATE TABLE IF NOT EXISTS `db_aircare`.`news` (
+DROP TABLE IF EXISTS `gywulo_aircare`.`news`;
+CREATE TABLE IF NOT EXISTS `gywulo_aircare`.`news` (
   `id` int NOT NULL AUTO_INCREMENT,
   `image` varchar(355) NOT NULL,
   `title` varchar(255) NOT NULL,
@@ -245,7 +245,7 @@ CREATE TABLE IF NOT EXISTS `db_aircare`.`news` (
 -- Déchargement des données de la table `news`
 --
 
-INSERT INTO `db_aircare`.`news` (`id`, `image`, `title`, `description`, `link`) VALUES
+INSERT INTO `gywulo_aircare`.`news` (`id`, `image`, `title`, `description`, `link`) VALUES
 (1, 'https://www.respire-asso.org/wp-content/uploads/2016/03/Tout-savoir-sur-lair-respire.jpg', 'Qualité de l\'air', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec rutrum augue. Donec nisl nulla, facilisis non velit eget, venenatis volutpat nisi. Nullam at sem et odio posuere varius id elementum nulla. Nam egestas porta nulla ut posuere. Integer vestibulum id nibh ut sodales. Nunc lacinia mauris malesuada nulla varius, sit amet euismod urna sagittis. Integer fringilla elit sit amet arcu luctus cursus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac', 'https://www.francebleu.fr/infos/environnement/lair-que-vous-respirez-en-gironde-est-il-de-bonne-qualite-1571236617'),
 (3, 'https://urbanlab.parisandco.paris/var/site/storage/images/_aliases/social_network_image/1/3/3/0/40331-3-fre-FR/Qualite-Air-couleurs-2.png', 'Lorem ipsum', 'Suspendisse pretium, turpis nec efficitur posuere, nisl diam condimentum lacus, sit amet egestas purus massa id erat. Praesent non sodales metus. Maecenas est lectus, fringilla ac libero eu, commodo pellentesque arcu. Phasellus commodo nisi non tempus elementum. Maecenas congue laoreet leo convallis rutrum.', 'https://actualite.lachainemeteo.com/actualite-meteo/2020-03-28/qualite-de-l-air-degradee-ce-week-end-au-nord-54577'),
 (4, 'https://www.maslacq.fr/mod_turbolead/upload//image/contenu/qualitair.jpg', 'Lorem ipsum', 'Etiam eu gravida diam. Sed fermentum dolor risus, a finibus elit semper sed. Nulla justo ligula, tempus vel lacus sit amet, vestibulum fermentum arcu. Quisque aliquet dui eget luctus aliquam. Quisque vulputate ipsum massa, ac malesuada diam imperdiet at. Duis eget accumsan ex. Donec hendrerit, ante vitae finibus tempus, metus odio mattis nibh, et dignissim quam urna vel enim.', 'https://www.maslacq.fr/qualite-de-l-air.php');
@@ -256,8 +256,8 @@ INSERT INTO `db_aircare`.`news` (`id`, `image`, `title`, `description`, `link`) 
 -- Structure de la table `nitrogen_dioxide`
 --
 
-DROP TABLE IF EXISTS `db_aircare`.`nitrogen_dioxide`;
-CREATE TABLE IF NOT EXISTS `db_aircare`.`nitrogen_dioxide` (
+DROP TABLE IF EXISTS `gywulo_aircare`.`nitrogen_dioxide`;
+CREATE TABLE IF NOT EXISTS `gywulo_aircare`.`nitrogen_dioxide` (
   `id` int NOT NULL AUTO_INCREMENT,
   `captor_id` int NOT NULL,
   `value` float NOT NULL,
@@ -269,7 +269,7 @@ CREATE TABLE IF NOT EXISTS `db_aircare`.`nitrogen_dioxide` (
 -- Déchargement des données de la table `nitrogen_dioxide`
 --
 
-INSERT INTO `db_aircare`.`nitrogen_dioxide` (`id`, `captor_id`, `value`, `percentage`) VALUES
+INSERT INTO `gywulo_aircare`.`nitrogen_dioxide` (`id`, `captor_id`, `value`, `percentage`) VALUES
 (1, 2, 213, 100);
 
 -- --------------------------------------------------------
@@ -278,8 +278,8 @@ INSERT INTO `db_aircare`.`nitrogen_dioxide` (`id`, `captor_id`, `value`, `percen
 -- Structure de la table `topics`
 --
 
-DROP TABLE IF EXISTS `db_aircare`.`topics`;
-CREATE TABLE IF NOT EXISTS `db_aircare`.`topics` (
+DROP TABLE IF EXISTS `gywulo_aircare`.`topics`;
+CREATE TABLE IF NOT EXISTS `gywulo_aircare`.`topics` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `date` text NOT NULL,
@@ -293,7 +293,7 @@ CREATE TABLE IF NOT EXISTS `db_aircare`.`topics` (
 -- Déchargement des données de la table `topics`
 --
 
-INSERT INTO `db_aircare`.`topics` (`id`, `title`, `date`, `user_id`, `user_name`, `message`) VALUES
+INSERT INTO `gywulo_aircare`.`topics` (`id`, `title`, `date`, `user_id`, `user_name`, `message`) VALUES
 (1, 'Qualité de l\'air Paris 15', '2021-12-29 20:15:40', 9, 'Jean Dupont', 'Test de message '),
 (3, 'Test de sujet', '2021-12-01 21:47:', 15, 'Adrien Bethuel', 'test message encore et encore'),
 (5, 'Créons un topic', '2021-12-05 22:41:', 15, 'Adrien Bethuel', 'Ceci est le message du topic créé<br />\r\nEn effet'),
@@ -306,8 +306,8 @@ INSERT INTO `db_aircare`.`topics` (`id`, `title`, `date`, `user_id`, `user_name`
 -- Structure de la table `users`
 --
 
-DROP TABLE IF EXISTS `db_aircare`.`users`;
-CREATE TABLE IF NOT EXISTS `db_aircare`.`users` (
+DROP TABLE IF EXISTS `gywulo_aircare`.`users`;
+CREATE TABLE IF NOT EXISTS `gywulo_aircare`.`users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `first_name` varchar(128) NOT NULL,
   `last_name` varchar(128) NOT NULL,
@@ -321,7 +321,7 @@ CREATE TABLE IF NOT EXISTS `db_aircare`.`users` (
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `db_aircare`.`users` (`id`, `first_name`, `last_name`, `email`, `password`, `role`) VALUES
+INSERT INTO `gywulo_aircare`.`users` (`id`, `first_name`, `last_name`, `email`, `password`, `role`) VALUES
 (16, 'Adrien', 'Bethuel', 'adrien.bethuel@eleve.isep.fr', '$2y$10$INymdOIP4LH.e.wjCdcxQeNlZcv1IMFp6A0ec/DQQXl.wjXda/g5K', 'administrator'),
 (27, 'Prénom', 'Nom', 'nom.prenom@gmail.com', '$2y$10$jDnzFpKiTL5Qc5fkCXStMu8fI3bVAkhuYeB5AcLJEOo.MY.HqVIl6', 'user'),
 (26, 'Paul', 'Max', 'max@mail.com', '$2y$10$Q/LJqMhNPQZLd50pM6KvMOwQ551/fMNr.wWJ/G6JERe4nUlU0zrn.', 'user'),
