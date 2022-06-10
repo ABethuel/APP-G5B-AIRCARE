1f5b03f9d168726aa3a85c78f67f67aacc87c6f4
Adrien Bethuel <adrien.bethuel@eleve.isep.fr>
2022-06-10 13:26:55 +0200

    db

--- capteurs_liste.php
+++ capteurs_liste.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 function displayCaptors(){
     require("./config/database.php");
 
--- sql/db_aircare.sql
+++ sql/db_aircare.sql
@@ -142,7 +142,7 @@ CREATE TABLE IF NOT EXISTS `db_aircare`.`email` (
   `message` varchar(255) NOT NULL,
   `status` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `email`
